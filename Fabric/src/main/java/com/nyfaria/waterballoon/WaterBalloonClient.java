09a3a6e6e5ef01bd9404097c2103c1c8f63a5cf8
Nyfaria DragonMoose <kinaria3324@gmail.com>
2024-06-14 22:39:46 -0500

    setup
    Took 5 minutes

---
+++ Fabric/src/main/java/com/nyfaria/waterballoon/WaterBalloonClient.java
@@ -0,0 +1,11 @@
+package com.nyfaria.waterballoon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WaterBalloonClient implements ClientModInitializer {
+    
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
