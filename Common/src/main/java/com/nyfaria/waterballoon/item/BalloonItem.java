08d959de7fc23866eab151a76c44513bc59cb2ad
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-09-22 21:23:20 -0400

    Touch up back of balloon bazooka texture, play sound when balloon hits something
    Will need to test once officially updating the mod

--- Common/src/main/java/com/nyfaria/waterballoon/item/BalloonItem.java
+++ Common/src/main/java/com/nyfaria/waterballoon/item/BalloonItem.java
@@ -19,20 +19,22 @@ public class BalloonItem extends Item {
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        ItemStack $$3 = player.getItemInHand(hand);
+        ItemStack stackInHand = player.getItemInHand(hand);
         level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.EGG_THROW, SoundSource.PLAYERS, 0.5F, 0.4F / (level.getRandom().nextFloat() * 0.4F + 0.8F));
+
         if (!level.isClientSide) {
-            ThrownBalloon $$4 = new ThrownBalloon(player, level);
-            $$4.setItem($$3);
-            $$4.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
-            level.addFreshEntity($$4);
+            ThrownBalloon balloonEntity = new ThrownBalloon(player, level);
+            balloonEntity.setItem(stackInHand);
+            balloonEntity.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 1.5F, 1.0F);
+            level.addFreshEntity(balloonEntity);
         }
 
         player.awardStat(Stats.ITEM_USED.get(this));
+
         if (!player.getAbilities().instabuild) {
-            $$3.shrink(1);
+            stackInHand.shrink(1);
         }
 
-        return InteractionResultHolder.sidedSuccess($$3, level.isClientSide());
+        return InteractionResultHolder.sidedSuccess(stackInHand, level.isClientSide());
     }
 }
